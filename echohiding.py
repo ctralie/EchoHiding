350795b88e23e8d3913c50644b00f2010a48daf2
Chris Tralie <chris.tralie@gmail.com>
2024-02-09 11:20:07 -0500

    Fixing 'echoes from the future' bug

--- echohiding.py
+++ echohiding.py
@@ -42,9 +42,10 @@ def echo_hide(x, L, b, delta0=50, delta1=75, alpha=0.4):
     """
     if x.size < L*b.size:
         raise ValueError("Error: Audio size {} is not long enough to hold {} bits of length {}".format(x.size, b.size, L))
-    x0  = np.array(x[0:-delta1])
-    x0 += alpha*x[delta0:delta0+x0.size]
-    x1  = x[0:-delta1] + alpha*x[delta1::]
+    x0  = np.array(x)
+    x0[delta0:] += alpha*x[0:-delta0]
+    x1  = np.array(x)
+    x1[delta1:] += alpha*x[0:-delta1]
     m = get_hann_mixer(L, b)
     return m*x1[0:m.size] + (1-m)*x0[0:m.size]
 
@@ -79,80 +80,38 @@ def extract_echo_bits(y, L, delta0=50, delta1=75):
     F = np.fft.irfft(np.log(F+1e-8), axis=1)
     return F[:, delta1] > F[:, delta0]
 
-def get_odg_distortion(x, y, sr, advanced=True, cleanup=True):
+def echo_hide_single(x, delta, alpha=0.4):
     """
-    A wrapper around GstPEAQ for computing objective measurements
-    of pereceived audio quality.
-    Software must be installed first:
-    https://github.com/HSU-ANT/gstpeaq
+    Put a single echo at a particular lag
 
     Parameters
     ----------
     x: ndarray(N)
-        Reference audio
-    y: ndarray(N)
-        Test audio
-    sr: int
-        Sample rate
-    advanced: bool
-        If True, use "advanced mode"
-    
-    Returns
-    -------
-    odg: float
-        Objective difference grade
-             0 - impairment imperceptible
-            -1 - impairment perceptible but not annoying
-            -2 - impairment slightly annoying
-            -3 - impairment annoying
-            -4 - impairment very annoying
-    di: float
-        Distortion index
+        Audio samples
+    delta: int
+        Delay of the echo
+    alpha: float
+        Amplitude of echo
     """
-    from scipy.io import wavfile
-    import os
-    from subprocess import check_output
-    ref = np.array(x*32768, dtype=np.int16)
-    wavfile.write("ref.wav", sr, ref)
+    y  = np.array(x)
+    y[delta:] += alpha*x[0:-delta]
+    return y
 
-    test = np.array(y*32768, dtype=np.int16)
-    wavfile.write("test.wav", sr, test)
-    if advanced:
-        res = check_output(["peaq", "--advanced", "ref.wav", "test.wav"])
-    else:
-        res = check_output(["peaq", "ref.wav", "test.wav"])
-    odg = float(str(res).split("\\n")[0].split()[-1])
-    di = float(str(res).split("\\n")[1].split()[-1])
-    if cleanup:
-        os.remove("ref.wav")
-        os.remove("test.wav")
-    return odg, di
-
-def get_mp3_encoded(x, sr, bitrate):
+def get_cepstrum(x):
     """
-    Get an mp3 encoding.  Assumes ffmpeg is installed and accessible
-    in the terminal environment
+    Compute the cepstrum of an entire chunk of audio
 
     Parameters
     ----------
-    x: ndarray(N, dtype=float)
-        Mono audio samples in [-1, 1]
-    sr: int
-        Sample rate
-    bitrate: int
-        Number of kbits per second to use in the mp3 encoding
+    x: ndarray(N)
+        Audio samples
+    
+    Returns
+    -------
+    ndarray(N)
+        Cepstrum
     """
-    import subprocess
-    import os
-    from scipy.io import wavfile
-    x = np.array(x*32768, dtype=np.int16)
-    wavfile.write("temp.wav", sr, x)
-    if os.path.exists("temp.mp3"):
-        os.remove("temp.mp3")
-    subprocess.call(["ffmpeg", "-i", "temp.wav","-b:a", "{}k".format(bitrate), "temp.mp3"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    os.remove("temp.wav")
-    subprocess.call(["ffmpeg", "-i", "temp.mp3", "temp.wav"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
-    os.remove("temp.mp3")
-    _, y = wavfile.read("temp.wav")
-    os.remove("temp.wav")
-    return y/32768
+    x = x*hann(x.size)
+    F = np.abs(np.fft.rfft(x, axis=1))
+    F = np.fft.irfft(np.log(F+1e-8), axis=1)
+    return F
